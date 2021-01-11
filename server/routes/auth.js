dfa982cdc54c3c3b7190af5ab9038b3a617103ae
Akinori Sakakibara <gorion.1213@gmail.com>
2021-01-11 22:32:37 +0900

    Implement the connect with api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const User = require('../models/user');
+const verifyToken = require('../middlewares/verify-token');
 
 const jwt = require('jsonwebtoken');
 
+/* Signup Route */
 router.post('/auth/signup', async (req, res) => {
   if (!req.body.email || !req.body.password) {
     res.json({ success: false, message: 'Please enter email or password' });
@@ -28,6 +30,55 @@ router.post('/auth/signup', async (req, res) => {
         message: err.message
       });
     }
+  }
+});
+
+/* Profile  Route */
+router.get('/auth/user', verifyToken, async (req, res) => {
+  try {
+    let foundUser = await User.findOne({ _id: req.decoded._id });
+    if (foundUser) {
+      res.json({
+        success: true,
+        user: foundUser
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+/* Login Route */
+router.post('/auth/login', async (req, res) => {
+  try {
+    let foundUser = await User.findOne({ email: req.body.email });
+    if (!foundUser) {
+      res.status(403).json({
+        success: false,
+        message: 'Anthentication failed, User not found'
+      })
+    } else {
+      if (foundUser.comparePassword(req.body.password)) {
+        let token = jwt.sign(foundUser.toJSON(), process.env.SECRET, {
+          expiresIn: 604800 // 1 week
+        })
+
+        res.json({ success: true, token: token });
+      } else {
+        res.status(403).json({
+          success: false,
+          message: 'Authentication failed, Wrong password!'
+        })
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
   }
 });
 
