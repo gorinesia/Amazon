f04001d42e538bc649712ff08c6be6a810a363ac
Akinori Sakakibara <gorion.1213@gmail.com>
2021-01-13 08:41:16 +0900

    Implemented the payment and order page

--- server/routes/order.js
+++ server/routes/order.js
@@ -4,8 +4,19 @@ const verifyToken = require('../middlewares/verify-token');
 
 router.get('/orders', verifyToken, async (req, res) => {
   try {
-    let products = await Order.find({ owner: req.decoded._id }).populate('owner products.productID, ')
+    let products = await Order.find({ owner: req.decoded._id })
+      .deepPopulate('owner products.productID.owner')
+      .exec();
+    res.json({
+      success: true,
+      products: products
+    });
   } catch (err) {
-    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
   }
-})
+});
+
+module.exports = router;
